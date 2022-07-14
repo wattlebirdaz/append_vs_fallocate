4eea70e1ed28fb480b25c59373a18b12972199c0
wattlebirdaz_optiplex <wattlebirdaz@gmail.com>
2022-07-14 16:29:09 +0900

    fsync to fdatasync & remove punchhole

--- allocate_write.cpp
+++ allocate_write.cpp
@@ -2,16 +2,18 @@
 #include <string>
 #include <unistd.h>
 
+static constexpr int val_size = 1024;
+char hoge[val_size] = {};
+
 int main() {
   int fd = open("allocate_write", O_WRONLY | O_TRUNC | O_CREAT, 0644);
   if (fd < 0)
     throw;
-  int val = 5000;
-  fallocate(fd, FALLOC_FL_KEEP_SIZE | FALLOC_FL_PUNCH_HOLE, 0, 23890);
-  for (int i = 0; i < val; i++) {
-    std::string what_to_write = std::to_string(i) + "\n";
-    write(fd, what_to_write.c_str(), what_to_write.size());
-    fsync(fd);
+  int n = 5000;
+  fallocate(fd, FALLOC_FL_KEEP_SIZE, 0, n * val_size);
+  for (int i = 0; i < n; i++) {
+    write(fd, hoge, val_size);
+    fdatasync(fd);
   }
   close(fd);
 }
--- append.cpp
+++ append.cpp
@@ -2,15 +2,17 @@
 #include <string>
 #include <unistd.h>
 
+static constexpr int val_size = 1024;
+char hoge[val_size] = {};
+
 int main() {
   int fd = open("append", O_WRONLY | O_APPEND | O_TRUNC | O_CREAT, 0644);
   if (fd < 0)
     throw;
-  int val = 5000;
-  for (int i = 0; i < val; i++) {
-    std::string what_to_write = std::to_string(i) + "\n";
-    write(fd, what_to_write.c_str(), what_to_write.size());
-    fsync(fd);
+  int n = 5000;
+  for (int i = 0; i < n; i++) {
+    write(fd, hoge, val_size);
+    fdatasync(fd);
   }
   close(fd);
 }
